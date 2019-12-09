86fc226d0036246adad110cda13684f7b4cecccd
Xan the Dragon <subzeroxan@gmail.com>
2019-12-08 21:48:50 -0600

    Add typed variant

--- XanBotCore/Exceptions/NonSingularResultException.cs
+++ XanBotCore/Exceptions/NonSingularResultException.cs
@@ -7,7 +7,8 @@ using System.Threading.Tasks;
 namespace XanBotCore.Exceptions {
 
 	/// <summary>
-	/// An exception that is thrown when there is not exclusively one return value possible from a function, for instance, in a function that is intended to get a user object from string.
+	/// A generic exception that is thrown when there is not exclusively one return value possible from a function, for instance, in a function that is intended to get a user object from string.<para/>
+	/// If the return value is of a definite type, consider using <see cref="NonSingularResultException{T}"/> instead.
 	/// </summary>
 	public class NonSingularResultException : Exception {
 
@@ -38,5 +39,37 @@ namespace XanBotCore.Exceptions {
 			PotentialReturnValuesString = retAsString;
 		}
 
+	}
+
+	/// <summary>
+	/// An exception that is thrown when there is not exclusively one return value possible from a function, for instance, in a function that is intended to get a user object from string.
+	/// </summary>
+	public class NonSingularResultException<T> : Exception {
+		/// <summary>
+		/// The message accompanying this NonSingularResultException.
+		/// </summary>
+		public new string Message { get; }
+
+		/// <summary>
+		/// An array of the potential return values.
+		/// </summary>
+		public T[] PotentialReturnValues { get; }
+
+		/// <summary>
+		/// An array of the potential return values, where each object has had its ToString method called.
+		/// </summary>
+		public string[] PotentialReturnValuesString { get; }
+
+		public NonSingularResultException(string message = null, params T[] potentialReturnValues) {
+			Message = message;
+			PotentialReturnValues = potentialReturnValues;
+
+			string[] retAsString = new string[potentialReturnValues.Length];
+			for (int idx = 0; idx < potentialReturnValues.Length; idx++) {
+				object obj = potentialReturnValues[idx];
+				retAsString[idx] = obj.ToString();
+			}
+			PotentialReturnValuesString = retAsString;
+		}
 	}
 }
