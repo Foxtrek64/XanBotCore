bf4e8ab2d14f37ac4c7879fc641448168f9f1ec2
Xan the Dragon <subzeroxan@gmail.com>
2019-12-08 22:46:31 -0600

    Add ExpireTimeMilliseconds parameter that will auto-delete a discord message for this error after the specified amount of time.

--- XanBotCore/Exceptions/CommandException.cs
+++ XanBotCore/Exceptions/CommandException.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DSharpPlus.Entities;
 using XanBotCore.CommandSystem;
 using XanBotCore.CommandSystem.Commands.ArchonCommands;
 
@@ -23,9 +24,29 @@ namespace XanBotCore.Exceptions {
 		/// </summary>
 		public Command Command { get; }
 
-		public CommandException(Command source, string errMsg) {
+		/// <summary>
+		/// True if the command message should be deleted, in which case the amount of time is specified by <see cref="ExpireTimeMilliseconds"/>
+		/// </summary>
+		public bool Expires { get; }
+
+		/// <summary>
+		/// The amount of milliseconds until the sent <see cref="DiscordMessage"/> reflecting this command is deleted, if applicable.
+		/// </summary>
+		public int ExpireTimeMilliseconds { get; } = 0;
+
+		/// <summary>
+		/// Create a new CommandException with the specified command source, error message, and optional expire time in milliseconds (which deletes the message sent in the <see cref="DiscordChannel"/> the command was executed in, if applicable.)
+		/// </summary>
+		/// <param name="source">The command that threw this error. Should always be <see cref="this"/></param>
+		/// <param name="errMsg">The error message to accompany which describes why this exception was thrown.</param>
+		/// <param name="expireTimeMilliseconds">The amount of milliseconds until the message sent is deleted. This does nothing if this command was executed from the console.</param>
+		public CommandException(Command source, string errMsg, int? expireTimeMilliseconds = null) {
 			Command = source;
 			Message = errMsg;
+			Expires = expireTimeMilliseconds != null;
+			if (Expires) {
+				ExpireTimeMilliseconds = expireTimeMilliseconds.Value;
+			}
 		}
 
 	}
