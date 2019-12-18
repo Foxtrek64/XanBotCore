3012a9bd0532b0b9993c09e98c358e5937ac3846
Xan <subzeroxan@gmail.com>
2019-12-17 21:49:09 -0600

    Usage example for the new VT system

--- EXAMPLE_BOT/Program.cs
+++ EXAMPLE_BOT/Program.cs
@@ -12,8 +12,39 @@ namespace EXAMPLE_BOT {
 
 		private const string BOT_TOKEN = "asdfg";
 
+		private static readonly ConsoleColorVT MTN_DEW_YELLOW = new ConsoleColorVT(128, 255, 63);
+
+		static void ThrowTestException() {
+			throw new InvalidOperationException("This is an exception message.");
+		}
+
+		/// <summary>
+		/// Tests the newly added VT Sequence system.<para/>
+		/// VT Sequences can be activated traditionally (\x1b[0;1m), via <see cref="ConsoleColorVT"/> (for easy designation of RGB colors), or via an alias ^#......; where ...... is a hex color code.<para/>
+		/// The legacy systems using § will still work too.
+		/// </summary>
+		static void DoVTConsoleTest() {
+			// If the following block errors and/or you aren't running a version of Windows 10 after the october 2018 update, remove these following lines.
+			XanBotLogger.EnableVTSupport();
+			XanBotLogger.ForegroundColor = MTN_DEW_YELLOW;
+			XanBotLogger.WriteLine("Dhue the dieu. This should be some weird yellow-green color.");
+			XanBotLogger.ForegroundColor = ConsoleColor.White;
+			XanBotLogger.WriteLine("Nice. If this is white, the backwards compatibility with setting directly to ConsoleColor worked.");
+			XanBotLogger.WriteLine("§cAnd if this is red, the legacy color formatting with alt+21 is working.");
+			try {
+				ThrowTestException();
+			} catch (Exception ex) {
+				XanBotLogger.WriteException(ex);
+			}
+			
+			XanBotLogger.WriteLine("^#FFFFFF;AdBlo\x1b[4mn\x1b[24mk disabaled.,. You may now press any key to comtue to next ^#FF0000;d^#FF8000;i^#FFFF00;m^#80FF00;e^#00FF00;n^#00FF80;s^#00FFFF;i^#0080FF;o^#0000FF;n^#8000FF;a^#FF00FF;l^#FF0080;s^#FFFFFF;.");
+			Console.ReadKey(true);
+		}
+
 		static void Main(string[] args) {
 			Console.Title = "Optional console title here.";
+
+			DoVTConsoleTest();
 
 			try {
 				MainAsync(args).ConfigureAwait(false).GetAwaiter().GetResult();
