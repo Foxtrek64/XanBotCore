fe63e5d41e5c724819eafd9af9231a3767dcec78
Xan <subzeroxan@gmail.com>
2019-12-22 13:44:24 -0600

    Some new formatting methods

--- XanBotCore/UserObjects/DiscordUserExtensions.cs
+++ XanBotCore/UserObjects/DiscordUserExtensions.cs
@@ -17,6 +17,17 @@ namespace XanBotCore.UserObjects {
 		}
 
 		/// <summary>
+		/// Return this <see cref="DiscordMember"/>'s name formatted as: [Nickname] Username#Discriminator. If the user has no nickname, it returns <see cref="GetFullName(DiscordUser)"/> instead (which has no nickname)
+		/// </summary>
+		/// <returns></returns>
+		public static string GetFullName(this DiscordMember member) {
+			if (member.Nickname != null && member.Nickname != default && member.Nickname != "") {
+				return "[" + member.Nickname + "] " + member.Username + "#" + member.Discriminator;
+			}
+			return GetFullName((DiscordUser)member);
+		}
+
+		/// <summary>
 		/// Formats this <see cref="DiscordUser"/>'s display name based on the specified <see cref="DisplayType"/>
 		/// </summary>
 		/// <param name="showAs">The <see cref="DisplayType"/> used to determine how the name is formatted.</param>
@@ -24,14 +35,29 @@ namespace XanBotCore.UserObjects {
 		public static string GetFormattedUser(this DiscordUser user, DisplayType showAs = DisplayType.UserId) {
 			string extra = "";
 			if (showAs == DisplayType.UserId) {
-				extra = $" (UserID {user.Id})";
+				extra = $" (UserID `{user.Id}`)";
 			} else if (showAs == DisplayType.Mention) {
 				extra = $" ({user.Mention})";
 			} else if (showAs == DisplayType.UserIdAndMention) {
-				extra = $" (UserID {user.Id} | {user.Mention})";
+				extra = $" (UserID `{user.Id}` | {user.Mention})";
 			}
 
 			return GetFullName(user) + extra;
+		}
+
+		public static string GetFormattedMember(this DiscordMember member, DisplayType showAs = DisplayType.NicknameUserId) {
+			string extra = "";
+			if (showAs == DisplayType.NicknameUserId) {
+				extra = $" (UserID `{member.Id})`";
+			} else if (showAs == DisplayType.NicknameMention) {
+				extra = $" ({member.Mention})";
+			} else if (showAs == DisplayType.NicknameUserIdAndMention) {
+				extra = $" (UserID `{member.Id}` | {member.Mention})";
+			} else {
+				return GetFormattedUser(member, showAs);
+			}
+
+			return GetFullName(member) + extra;
 		}
 	}
 
@@ -45,7 +71,7 @@ namespace XanBotCore.UserObjects {
 		None,
 
 		/// <summary>
-		/// Username#Discriminator (UserID id_here)
+		/// Username#Discriminator (UserID `id_here`)
 		/// </summary>
 		UserId,
 
@@ -55,8 +81,26 @@ namespace XanBotCore.UserObjects {
 		Mention,
 
 		/// <summary>
-		/// Username#Discriminator (UserID id_here | &lt;@id_here&gt;)
+		/// Username#Discriminator (UserID `id_here` | &lt;@id_here&gt;)
 		/// </summary>
-		UserIdAndMention
+		UserIdAndMention,
+
+		/// <summary>
+		/// CAN ONLY BE USED ON MEMBERS, NOT USERS.<para/>
+		/// [Nickname] Username#Discriminator (UserID `id_here`)
+		/// </summary>
+		NicknameUserId,
+
+		/// <summary>
+		/// CAN ONLY BE USED ON MEMBERS, NOT USERS.<para/>
+		/// [Nickname] Username#Discriminator (&lt;@id_here&gt;)
+		/// </summary>
+		NicknameMention,
+
+		/// <summary>
+		/// CAN ONLY BE USED ON MEMBERS, NOT USERS.<para/>
+		/// [Nickname] Username#Discriminator (UserID `id_here` | &lt;@id_here&gt;)
+		/// </summary>
+		NicknameUserIdAndMention,
 	}
 }
